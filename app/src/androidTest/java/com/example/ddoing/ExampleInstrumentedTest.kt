bf1316e16d17ef5cc9c9f0d3b546d53309743e17
rfvtgbyh11 <8174188@naver.com>
2023-01-19 00:48:06 +0900

    hyunsu

--- app/src/androidTest/java/com/example/ddoing/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/ddoing/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.eos.ddoing
+package com.example.ddoing
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.eos.ddoing", appContext.packageName)
+        assertEquals("com.example.ddoing", appContext.packageName)
     }
 }
