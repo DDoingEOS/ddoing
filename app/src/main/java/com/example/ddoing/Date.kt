b4212c103bc7b3050e9a13a16b0ec1b472c8371b
rfvtgbyh11 <8174188@naver.com>
2023-01-24 18:55:31 +0900

    1

--- app/src/main/java/com/example/ddoing/Date.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ddoing
-
-/**
- * Plan 받아올때 쓸수도있는 객체 (우선 리사이클러뷰에 쓸것들만 넣어놓음..)
- */
-data class Date(var todoKey: String, var todoName: String, var alertTime: String)
--- app/src/main/java/com/example/ddoing/model/Plan.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ddoing.model
-
-data class Plan(val stringresourceId: Int) {
-
-}
